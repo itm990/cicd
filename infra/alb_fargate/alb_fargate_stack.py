1ce9f0ef75e65cea73050a72b43f367701585173
tomokiikawa <ikawatomoki91011@gmail.com>
2023-04-23 18:52:52 +0900

    Modify infra

--- infra/alb_fargate/alb_fargate_stack.py
+++ infra/alb_fargate/alb_fargate_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_ecs_patterns as ecs_patterns,
+    aws_ecr_assets as ecr_assets,
 )
 from constructs import Construct
 
@@ -15,11 +16,15 @@ class AlbFargateStack(Stack):
 
         cluster = ecs.Cluster(self, "Cluster", vpc=vpc)
 
+        asset = ecr_assets.DockerImageAsset(self, "petgram",
+            directory="../app"
+        )
+
         ecs_patterns.ApplicationLoadBalancedFargateService(self, "Service",
             cluster=cluster,
             cpu=512,
             desired_count=6,
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_registry('amazon/amazon-ecs-sample')),
+                image=ecs.ContainerImage.from_docker_image_asset(asset)),
             memory_limit_mib=2048,
             public_load_balancer=True)
