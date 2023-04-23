43324f0684a52704a7c7517808a098a9323b9ce1
tomokiikawa <ikawatomoki91011@gmail.com>
2023-04-23 17:16:28 +0900

    Add app and infra files

---
+++ infra/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from alb_fargate.alb_fargate_stack import AlbFargateStack
+
+
+app = cdk.App()
+AlbFargateStack(app, "InfraStack",
+
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    )
+
+app.synth()
